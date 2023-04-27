d490c6a3a564d5dd290e8db11b174647db534f1c
Kanishk <kanishkjha10@gmail.com>
2023-04-27 11:33:42 +0530

    Moved a bit forward

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
-import { View, Text, TextInput, TouchableOpacity, Image, Flatlist } from 'react-native'
-import { useRouter } from 'expo-router'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
-import { icons, SIZES } from "../../../constants"
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
-  const router = useRouter()
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
+  const router = useRouter();
   return (
     <View>
       <View style={styles.container}>
@@ -15,15 +26,41 @@ const Welcome = () => {
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} value="" onChange={() => { }} placeholder="What are you looking for" />
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for"
+          />
         </View>
 
         <TouchableOpacity style={styles.searchBtn}>
-          <Image source={icons.search} resizeMode="contain" style={styles.searchBtnImage} />
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
         </TouchableOpacity>
       </View>
-    </View>
-  )
-}
 
-export default Welcome
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
